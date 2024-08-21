00a981e9a85671a1f8de95418c7c01aaac5b9b7f
Bill Qubeka <Bill Qubeka>
2024-08-21 10:21:16 +0200

    Change location of UserContext.js

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { UserContext } from './Contexts/UserContext';
+import { UserContext } from './components/UserContext';
 import ProfilePage from './components/ProfilePage';
 
 function App() {
