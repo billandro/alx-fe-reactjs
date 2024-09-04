810db93decbb4833dc75a4d388ec61e39df3861e
Bill Qubeka <Bill Qubeka>
2024-09-04 23:00:08 +0200

    Add BlogPost component

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,19 +1,26 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link, useParams } from 'react-router-dom'
 import Profile from './components/Profile';
 import ProfileForm from './components/ProfileForm';
 import ProfileDetails from './components/ProfileDetails';
+import BlogPost from './components/BlogPost';
 
 function App() {
   return (
     <Router>
       <div>
         <nav>
+          <ul>
+            <li>
+              <Link to="/blog/:id">Blog</Link>
+            </li>
+          </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Profile />} />
           <Route path="/profile/*" element={<Profile />} />
           <Route path="/profile-form" element={<ProfileForm />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
         </Routes>
       </div>
     </Router>
