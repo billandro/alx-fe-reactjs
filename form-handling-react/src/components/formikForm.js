343007d9faf2299da62b6e6f9f4d50b79ea8f0c7
Bill Qubeka <Bill Qubeka>
2024-09-04 10:51:30 +0200

    Change structure of form

---
+++ form-handling-react/src/components/formikForm.js
@@ -0,0 +1,48 @@
+// src/components/formikForm.js
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+// Define the validation schema with Yup
+const validationSchema = Yup.object({
+    username: Yup.string().required("Username is required"),
+    email: Yup.string().email("Invalid email format").required("Email is required"),
+    password: Yup.string().required("Password is required"),
+});
+
+const FormikForm = () => {
+  const handleSubmit = (values) => {
+    console.log("Form Submitted", values);
+  };
+
+  return (
+    <Formik
+      initialValues={{ username: "", email: "", password: "" }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {() => (
+        <Form>
+          <div>
+            <label>Username:</label>
+            <Field name="username" type="text" />
+            <ErrorMessage name="username" component="div" style={{ color: "red" }} />
+          </div>
+          <div>
+            <label>Email:</label>
+            <Field name="email" type="email" />
+            <ErrorMessage name="email" component="div" style={{ color: "red" }} />
+          </div>
+          <div>
+            <label>Password:</label>
+            <Field name="password" type="password" />
+            <ErrorMessage name="password" component="div" style={{ color: "red" }} />
+          </div>
+          <button type="submit">Register</button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default FormikForm;
